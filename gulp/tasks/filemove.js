5b31e50a68bea9c6b6f4c4664d1e86e27be880d2
caranicas <kendrick.caranicas@gmail.com>
2014-11-03 21:32:59 -0800

    remove unused physijs files

--- gulp/tasks/filemove.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config').physmove
-
-gulp.task('filemove', function() {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest));
-});
--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var config = require('../config').markup
 
-gulp.task('markup', ['filemove'], function() {
+gulp.task('markup', function() {
   return gulp.src(config.src)
     .pipe(gulp.dest(config.dest));
 });
